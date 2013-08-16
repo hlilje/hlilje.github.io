9b955c6704ab7e587750755868567a812c9e3ba0
Hampus Liljekvist <hlilje@server.fake>
2013-08-16 20:50:55 +0200

    jekyll!

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -1,5 +1,6 @@
 var metas = document.getElementsByTagName('meta');
 var i;
+
 if (navigator.userAgent.match(/iPhone/i)) {
   for (i=0; i<metas.length; i++) {
     if (metas[i].name == "viewport") {
@@ -8,6 +9,7 @@ if (navigator.userAgent.match(/iPhone/i)) {
   }
   document.addEventListener("gesturestart", gestureStart, false);
 }
+
 function gestureStart() {
   for (i=0; i<metas.length; i++) {
     if (metas[i].name == "viewport") {
